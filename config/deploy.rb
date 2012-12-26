ca42a575bea78d6d6381f63e0548b04889e7bae7
Francesco Canessa <makevoid@gmail.com>
2012-12-26 19:02:35 +0100

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :scm, "git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 
-set :password, File.read("/Users/makevoid/.password").strip.gsub(/33/, '')
+set :password, File.read(File.expand_path "~/.password").strip.gsub(/33/, '')
 
 
 set :user,        "www-data"
